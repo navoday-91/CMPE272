e44339d73dd772881fdd1493232b440f1d98e5dd
navoday-91 <navoday.91@gmail.com>
2017-10-19 16:15:08 -0500

    PyCharm Test

---
+++ test.php
@@ -0,0 +1,8 @@
+      <?php
+        $myfile = fopen("webcontacts.txt", "r") or die("Unable to open file!");
+        while (!feof($webcontacts))
+        {
+        $frec = fgets($myfile);
+        $info = explode(";",$frec);
+        print($info[0]);
+      ?>
