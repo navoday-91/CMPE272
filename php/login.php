0ec0d80948ae7f54ef6c3dda61ecab440b932b9a
navoday-91 <navoday.91@gmail.com>
2017-10-23 15:38:41 -0500

    PyCharm Test

--- php/login.php
+++ php/login.php
@@ -11,14 +11,14 @@ else
 $username=$_POST['username'];
 $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysql_connect("localhost", "root", "");
+$connection = mysql_connect("localhost", "root", "redhat");
 // To protect MySQL injection for Security purpose
 $username = stripslashes($username);
 $password = stripslashes($password);
 $username = mysql_real_escape_string($username);
 $password = mysql_real_escape_string($password);
 // Selecting Database
-$db = mysql_select_db("company", $connection);
+$db = mysql_select_db("abc", $connection);
 // SQL query to fetch information of registerd users and finds user match.
 $query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
 $rows = mysql_num_rows($query);
