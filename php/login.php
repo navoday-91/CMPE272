30f82f026c06f74702b91cf851f6028f5c482492
navoday-91 <navoday.91@gmail.com>
2017-10-24 23:00:54 -0500

    PyCharm Test

--- php/login.php
+++ php/login.php
@@ -1,11 +1,7 @@
 <?php
 session_start(); // Starting Session
-echo("You are here!");
 $error=''; // Variable To Store Error Message
 if (isset($_POST['Login'])) {
-$username=$_POST['user_username'];
-$password=$_POST['user_password'];
-echo($username + "     " + $password);
 if (empty($_POST['user_username']) || empty($_POST['user_password'])) {
 $error = "Username or Password is invalid";
 echo($error);
@@ -13,16 +9,14 @@ header("location: ../index.html"); // Redirecting back
 }
 else
 {
-echo('You are here in else' + '\n');
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $connection = mysqli_connect("localhost", "navoday", "redhat");
-echo($username + $password);
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
     echo('connection to db failed');
     echo($connection);
 }
-echo("Connected successfully");
+echo("Connected successfully \n");
 // To protect MySQL injection for Security purpose
 $username = stripslashes($username);
 $password = stripslashes($password);
@@ -31,6 +25,8 @@ $password = mysqli_real_escape_string($password);
 // Selecting Database
 $db = mysqli_select_db("abc", $connection);
 // SQL query to fetch information of registerd users and finds user match.
+echo($_POST['user_username']);
+echo($_POST['user_password']);
 $query = mysqli_query("select * from login where password='$password' AND username='$username'", $connection);
 $rows = mysqli_num_rows($query);
 if ($rows == 1) {
