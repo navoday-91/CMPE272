6e7b6affc7bc43e657d3c32bd5e8f389e0c85bd6
navoday-91 <navoday.91@gmail.com>
2017-10-25 13:38:55 -0500

    PyCharm Test

--- logindata.php
+++ logindata.php
@@ -31,12 +31,10 @@
             // SQL query to fetch information of registerd users and finds user match.
             $query = mysqli_query($connection, "select * from login;");
             $rows = mysqli_num_rows($query);
-            $result = mysqli_fetch_all($query);
-            echo($result[0]);
             if ($rows > 0) {
-                while ($row = mysql_fetch_assoc($result)) {
+                while ($user = $query->fetch_assoc()) {
         ?>
-        <li><?php echo($row["username"]); ?></li>
+        <li><?php echo($user["username"]); ?></li>
         <?php
                 }
             }
