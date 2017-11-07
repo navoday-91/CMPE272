1b093b85c37049738d1ce127b87e175fe5f12373
navoday-91 <navoday.91@gmail.com>
2017-11-06 16:12:46 -0800

    PyCharm Test

--- php/signup.php
+++ php/signup.php
@@ -83,7 +83,7 @@ else
             header("location: ../register.php"); // Redirecting To Registration Page
         }
         $query = mysqli_query($connection, "insert into userdata values('$username','$firstname','$lastname','$email','$address','$phone');");
-        echo(mysqli_error($query));
+        echo(mysqli_error($db));
         $_SESSION['error'] = "Registration Successful";
         //header("location: ../userlogin.php");
     } 
