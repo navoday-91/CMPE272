ea25e9a1f4ae538c411a27f8abe296e8dd50025e
navoday-91 <navoday.91@gmail.com>
2017-11-17 11:30:26 -0800

    CMPE281

---
+++ CMPE281/server.py
@@ -0,0 +1,89 @@
+import random
+import MySQLdb
+
+from autobahn.twisted.websocket import WebSocketServerProtocol, \
+    WebSocketServerFactory
+
+
+class MyServerProtocol(WebSocketServerProtocol):
+    liveclients = {}
+    clients = {}
+    i = 0
+    user = ""
+    def register(self, client, payload):
+        """
+        Add client to list of managed connections.
+        """
+        if self.i == 0:
+            self.user = payload.decode('utf-8').split(";")[0]
+            self.liveclients[self.user] = client
+            self.i += 1
+
+        #self.clients[client.peer] = {"object": client, "partner": None}
+
+    def unregister(self, client):
+        """
+        Remove client from list of managed connections.
+        """
+        del self.liveclients[self.user]
+
+    def findPartner(self, client):
+        """
+        Find chat partner for a client. Check if there any of tracked clients
+        is idle. If there is no idle client just exit quietly. If there is
+        available partner assign him/her to our client.
+        """
+        print("we are finding partners - ", [c for c in self.clients])
+        available_partners = [c for c in self.clients if c != client.peer and not self.clients[c]["partner"]]
+        if not available_partners:
+            print("no partners for {} check in a moment".format(client.peer))
+        else:
+            partner_key = random.choice(available_partners)
+            self.clients[partner_key]["partner"] = client
+            self.clients[client.peer]["partner"] = self.clients[partner_key]["object"]
+
+    def communicate(self, client, payload, isBinary):
+        sendto = payload.decode('utf-8').split(";")[1]
+        if sendto not in self.liveclients:
+            self.sendMessage(b"Sorry you don't have partner yet, check back in a minute")
+        else:
+            c = self.liveclients[sendto]
+            c.sendMessage(payload, isBinary)
+
+    def onConnect(self, request):
+        print("Client connecting: {0}".format(request.peer))
+        self.i = 0
+        #self.register(self)
+        #self.findPartner(self)
+
+    def onOpen(self):
+        print("WebSocket connection open.")
+
+    def onMessage(self, payload, isBinary):
+        if self.i == 0:
+            self.register(self, payload)
+        else:
+            self.communicate(self,payload,isBinary)
+        #self.sendMessage(payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        print("WebSocket connection closed: {0}".format(reason))
+        self.unregister(self)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    from twisted.python import log
+    from twisted.internet import reactor
+
+    log.startLogging(sys.stdout)
+    factory = WebSocketServerFactory(u"ws://127.0.0.1:9000")
+    factory.protocol = MyServerProtocol
+    # factory.setProtocolOptions(maxConnections=2)
+
+    # note to self: if using putChild, the child must be bytes...
+
+    reactor.listenTCP(9000, factory)
+    reactor.run()
