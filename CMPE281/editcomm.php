f647d1152f68660df09f0a41462d91bd0e9f1762
navoday-91 <navoday.91@gmail.com>
2017-11-20 15:52:42 -0800

    CMPE281

--- CMPE281/editcomm.php
+++ CMPE281/editcomm.php
@@ -329,7 +329,7 @@
                                                           <option value = ""> Select Community</option>
                                                         <?php if ($rows > 0) {
                                                             while ($user = $query->fetch_assoc()) { ?>
-                                                                <option value = "<?php echo($user['community_name']); ?>"> <?php echo($user['community_name']);
+                                                                <option value = "<?php echo($user['community_name']); ?>" <?php if ($user['community_name'] == $_SESSION['addmgrcomm']){echo('selected');} ?>> <?php echo($user['community_name']);
                                                         ?></option>
                                                         <?php } } 
                                                             else{?>
@@ -385,9 +385,25 @@
                                                 ?>
                                                 <form id="login_form" class="dialog-form" action="editcomm.php" method="POST">
                                                 <fieldset>
+                                                //Show Users inside community
                                                   <legend>Add a community manager</legend>
                                                   <div class="form-group">
-                                                    <label for="user_id" class="control-label">User ID:</label>
+                                                    <label for="new_mgr" class="control-label">User ID:</label>
+                                                    <select id="new_mgr" class="form-control" name = "new_mgr" autofocus> 
+                                                          <option value = ""> Select User To Add</option>
+                                                        <?php 
+                                                        $db = mysqli_select_db($connection, "cmpe281");
+                                                        // SQL query to fetch information of registerd users and finds user match.
+                                                        $query = mysqli_query($connection, "select `username` from login where community_name = '$commname' and role = 'citizen';");
+                                                        $rows = mysqli_num_rows($query);
+                                                        if ($rows > 0) {
+                                                            while ($user = $query->fetch_assoc()) { ?>
+                                                                <option value = "<?php echo($user['username']); ?></option>
+                                                        <?php } } 
+                                                            else{?>
+                                                                <option value = ""> No Users Available</option>
+                                                        <?php } ?>
+                                                    </select>
                                                     <input type="text" id="user_id" class="form-control" name="user_id" autofocus/>
                                                   </div>
                                 
@@ -408,6 +424,19 @@
                                                 <?php
                                           }
                                               ?>
+                                              
+                                              //Handle Adding manager
+                                              
+                                              <?php if (isset($_POST['Add'])) {
+                                                
+                                                $newmgr = ($_POST['new_mgr']);
+                                                
+                                                // Selecting Database
+                                                $db = mysqli_select_db($connection, "cmpe281");
+                                                // SQL query to fetch information of registerd users and finds user match.
+                                                $query = mysqli_query($connection, "update login set role = 'manager' where username = '$newmgr';");
+                                                $_SESSION['error2'] = "Manager Added";
+                                            ?>        
                                               
                                         </ul>
 </div></div></span>
