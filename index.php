a8dc35af53fd705f93947817e1ee27d6f50b664e
navoday-91 <navoday.91@gmail.com>
2017-12-06 12:08:06 -0800

    CMPE281

--- index.php
+++ index.php
@@ -56,6 +56,15 @@ if(!isset($_COOKIE[$cookie_name])) {
         .dropdown:hover .dropdown-content {
             display: block;
                             }
+        .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+}
+
 </style>
 </head>
 <?php
