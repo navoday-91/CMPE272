28a9b849f47724eb5a6ddb441aa19f246c309270
navoday-91 <navoday.91@gmail.com>
2017-12-06 12:25:03 -0800

    CMPE281

--- avboutique/abc.php
+++ avboutique/abc.php
@@ -7,7 +7,7 @@ $ch = curl_init();
 
 echo("Users from Navoday's Site:");
 curl_setopt($ch, CURLOPT_URL, "http://www.abc-consulting.tk/fetchusers.php");
-curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 $result = curl_exec($ch);
 
@@ -15,7 +15,7 @@ echo($result);
 
 echo("Users from Neha's Site:");
 curl_setopt($ch, CURLOPT_URL, "http://www.wonderarchitectures.ga/fetchusers.php");
-curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 $result = curl_exec($ch);
 
