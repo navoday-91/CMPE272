0314e71073773e994881c143960ccce939d31b6d
navoday-91 <navoday.91@gmail.com>
2017-12-06 12:33:17 -0800

    CMPE281

--- avboutique/abc.php
+++ avboutique/abc.php
@@ -5,21 +5,29 @@
 <?php
 $ch = curl_init();
 
-echo("Users from Navoday's Site:");
+echo("Users from Navoday's Site: <br/> ");
 curl_setopt($ch, CURLOPT_URL, "http://www.abc-consulting.tk/fetchusers.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 $result = curl_exec($ch);
 
-echo($result);
+echo($result."<br/>");
 
-echo("Users from Neha's Site:");
+echo("Users from Neha's Site:<br/>");
 curl_setopt($ch, CURLOPT_URL, "http://www.wonderarchitectures.ga/fetchusers.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 $result = curl_exec($ch);
 
-echo($result);
+echo($result."<br/>");
+
+echo("Users from Anav's Site:<br/>");
+curl_setopt($ch, CURLOPT_URL, "http://anavsharma.com/barkinghampalace/curlmydb/index.php");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+
+echo($result."<br/>");
 
 curl_close($ch);
 ?>
