3fd89a0ec2140c1bbb2501e7edb62489c1e92121
navoday-91 <navoday.91@gmail.com>
2017-12-07 18:03:48 -0800

    CMPE281

--- userlogin.php
+++ userlogin.php
@@ -13,104 +13,8 @@
 
 
 </head>
-<?php
-// Include FB config file && User class
-require_once 'config.php';
-require_once 'user.php';
 
-if(isset($accessToken)){
-    if(isset($_SESSION['facebook_access_token'])){
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    }else{
-        // Put short-lived access token in session
-        $_SESSION['facebook_access_token'] = (string) $accessToken;
 
-          // OAuth 2.0 client handler helps to manage access tokens
-        $oAuth2Client = $fb->getOAuth2Client();
-
-        // Exchanges a short-lived access token for a long-lived one
-        $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-        $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-
-        // Set default access token to be used in script
-        $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-    }
-
-    // Redirect the user back to the same page if url has "code" parameter in query string
-    if(isset($_GET['code'])){
-        header('Location: ./');
-    }
-
-    // Getting user facebook profile info
-    try {
-        $profileRequest = $fb->get('/me?fields=name,first_name,last_name,email,link,gender,locale,picture');
-        $fbUserProfile = $profileRequest->getGraphNode()->asArray();
-    } catch(FacebookResponseException $e) {
-        echo 'Graph returned an error: ' . $e->getMessage();
-        session_destroy();
-        // Redirect user back to app login page
-        header("Location: ./");
-        exit;
-    } catch(FacebookSDKException $e) {
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
-    }
-
-    // Initialize User class
-    $user = new User();
-
-    // Insert or update user data to the database
-    $fbUserData = array(
-        'oauth_provider'=> 'facebook',
-        'oauth_uid'     => $fbUserProfile['id'],
-        'first_name'    => $fbUserProfile['first_name'],
-        'last_name'     => $fbUserProfile['last_name'],
-        'email'         => $fbUserProfile['email'],
-        'gender'        => $fbUserProfile['gender'],
-        'locale'        => $fbUserProfile['locale'],
-        'picture'       => $fbUserProfile['picture']['url'],
-        'link'          => $fbUserProfile['link']
-    );
-    $userData = $user->checkUser($fbUserData);
-
-    // Put user data into session
-    $_SESSION['userData'] = $userData;
-
-    // Get logout url
-    $logoutURL = $helper->getLogoutUrl($accessToken, $redirectURL.'logout.php');
-
-    // Render facebook profile data
-    if(!empty($userData)){
-        $output  = '<h1>Facebook Profile Details </h1>';
-        $output .= '<img src="'.$userData['picture'].'">';
-        $output .= '<br/>Facebook ID : ' . $userData['oauth_uid'];
-        $output .= '<br/>Name : ' . $userData['first_name'].' '.$userData['last_name'];
-        $output .= '<br/>Email : ' . $userData['email'];
-        $output .= '<br/>Gender : ' . $userData['gender'];
-        $output .= '<br/>Locale : ' . $userData['locale'];
-        $output .= '<br/>Logged in with : Facebook';
-        $output .= '<br/><a href="'.$userData['link'].'" target="_blank">Click to Visit Facebook Page</a>';
-        $output .= '<br/>Logout from <a href="'.$logoutURL.'">Facebook</a>';
-    }else{
-        $output = '<h3 style="color:red">Some problem occurred, please try again.</h3>';
-    }
-
-}else{
-    // Get login url
-    $loginURL = $helper->getLoginUrl($redirectURL, $fbPermissions);
-
-    // Render facebook login button
-    $output = '<a href="'.htmlspecialchars($loginURL).'">Login with Facebook</a>';
-}
-?>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=345965562542579';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
 <div id="dialog" class="dialog dialog-effect-in">
   <div class="dialog-front">
@@ -175,7 +79,6 @@ if(isset($accessToken)){
           <div class="pad-btm-20">
             <input type="submit" class="btn btn-default btn-block btn-lg" value="Continue"/>
           </div>
-          <div class="fb-login-button" data-max-rows="1" data-size="medium" data-button-type="login_with" data-show-faces="true" data-auto-logout-link="true" data-use-continue-as="true"></div>
           <div class="text-center">
             <p>Return to <a href="#" class="link user-actions"><strong>log in page</strong></a>?</p>
           </div>
