a9660e9f5a3c6cd088c116e59b5306f1766abc23
navoday-91 <navoday.91@gmail.com>
2017-12-08 20:12:16 -0800

    CMPE281

--- market.php
+++ market.php
@@ -1047,11 +1047,11 @@ $services_dict = array(1 => "<a href = 'services/str_cnslt.php'>Strategy and Con
 </div><!--end row-wrapper -->
 <div class="row-fluid-wrapper row-depth-0 row-number-1 ">
 <div class="row-fluid ">
-    <div class="span12 widget-span widget-type-cell content-section columns-section two-column-right-section" style="" data-widget-type="cell" data-x="0" data-w="12">
+    <div class="span12 widget-span widget-type-cell content-section columns-section two-column-right-section" style="" data-widget-type="cell" data-x="0" data-w="15">
 
         <div class="row-fluid-wrapper row-depth-1 row-number-1 ">
         <div class="row-fluid ">
-            <div class="span12 widget-span widget-type-cell centered column-equal-height medium-stack" style="" data-widget-type="cell" data-x="0" data-w="12">
+            <div class="span12 widget-span widget-type-cell centered column-equal-height medium-stack" style="" data-widget-type="cell" data-x="0" data-w="15">
     <script language="javascript" type="text/javascript">
             var token = "<?php echo($_SESSION['jcartToken']) ?>";                        
                                         
