cb27613df5013dbb8fd8500eda2c9580c31176d3
navoday-91 <navoday.91@gmail.com>
2017-12-09 11:18:49 -0800

    CMPE281

--- productdisplay.php
+++ productdisplay.php
@@ -1123,9 +1123,57 @@ h1 { font-size: 1.5em; margin: 10px; }
     <div class="dialog-content">
       <form id="reviewform" class="dialog-form" action=<?php echo("php/subreview.php?id=".$id) ?> method="POST">
         <fieldset>
-          
+          <legend>Rating and Reviews</legend>
+          <div class="form-group">
+            <label for="review" class="control-label">Review</label>
+            <input type="text" id="review" class="form-control" name="review" autofocus/>
+          </div>
+          <div class="form-group">
+              <center>
+                    
+                    <fieldset class="rating">
+    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+    <input type="radio" id="star4half" name="rating" value="4.5" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+    <input type="radio" id="star3half" name="rating" value="3.5" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+    <input type="radio" id="star2half" name="rating" value="2.5" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+    <input type="radio" id="star1half" name="rating" value="1.5" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+    <input type="radio" id="starhalf" name="rating" value="0.5" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                </fieldset>
+                </center>
+          </div>
+          <br/>
+          <?php
+        $connection = mysqli_connect("localhost", "navoday", "redhat");
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+            echo('connection to db failed');
+            echo($connection);
+            }
+
+            // Selecting Database
+            $username = $_SESSION['login_user'];
+            $db = mysqli_select_db($connection, "abc");
+            // SQL query to fetch information of registerd users and finds user match.
+            $query = mysqli_query($connection, "select count(*) as count, avg(rating) as rating from rating where productid = '$id';");
+            $user = $query->fetch_assoc();
+        ?>
+        
+          <left><?php echo("Total ".$user['count']." Ratings.        Avg. Rating: ".$user['rating']) ?></left>
+          <br/>
+          <?php if (isset($_SESSION['error'])){ ?>
+          <div class="text-center pad-top-20">
+            <p><font color="red"><strong><?php echo($_SESSION['error']); ?></strong></font></p>
+          </div>
+          <?php 
+          unset($_SESSION['error']);
+          } ?>
+          <div class="pad-top-20 pad-btm-20">
             <input type="submit" class="btn btn-default btn-block btn-lg" name="Submit" value="Submit">
-    
+          </div>
         </fieldset>
       </form>
 				</center>
