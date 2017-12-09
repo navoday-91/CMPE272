dcb722c515c3752354268c2e7109d79285bd8a6f
navoday-91 <navoday.91@gmail.com>
2017-12-08 21:01:20 -0800

    CMPE281

--- market.php
+++ market.php
@@ -1047,7 +1047,7 @@ $services_dict = array(1 => "<a href = 'services/str_cnslt.php'>Strategy and Con
 </div><!--end row-wrapper -->
 <div class="row-fluid-wrapper row-depth-0 row-number-1 ">
 <div class="row-fluid ">
-    <div class="span12 widget-span widget-type-cell content-section columns-section two-column-right-section" style="" data-widget-type="cell" data-x="0" data-w="12">
+    <div class="span12 widget-span widget-type-cell content-section columns-section two-column-right-section" style="" data-widget-type="cell" data-x="5" data-w="12">
 
     <script language="javascript" type="text/javascript">
             var token = "<?php echo($_SESSION['jcartToken']) ?>";                        
