51a3b650c5e49ce1a3b5e1d701127bf842df6905
navoday-91 <navoday.91@gmail.com>
2017-12-09 08:37:27 -0800

    CMPE281

--- productdisplay.php
+++ productdisplay.php
@@ -653,8 +653,23 @@ $services_dict = array(1 => "<a href = 'services/str_cnslt.php'>Strategy and Con
     <meta name="author" content="ABC Solutions">
     <meta name="description" content="ABC Consulting provides analytics services and understands perspective required for building and implementation of Business and Market Data.">
     <meta name="generator" content="HubSpot">
-    <title>User Data</title>
+    <title>Product Description</title>
     
+    
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+<!--<link href="css/bootstrap.min.css" rel="stylesheet">-->	
+<link rel="stylesheet"  href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<link href="css/star-rating.css" media="all" rel="stylesheet" type="text/css" />
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/modal.js"></script>
+<script src="js/star-rating.js" type="text/javascript"></script>
+
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
 
     
     
@@ -881,7 +896,7 @@ $services_dict = array(1 => "<a href = 'services/str_cnslt.php'>Strategy and Con
                             <div class="span12 widget-span widget-type-header " style="" data-widget-type="header" data-x="0" data-w="12">
                                 <div class="cell-wrapper layout-widget-wrapper">
                                     <span id="hs_cos_wrapper_module_14509432248707604" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_header" style="" data-hs-cos-general-type="widget" data-hs-cos-type="header"><h1><span id="hs_cos_wrapper_name" class="hs_cos_wrapper hs_cos_wrapper_meta_field hs_cos_wrapper_type_text" style="" data-hs-cos-general-type="meta_field" data-hs-cos-type="text">
-                                        ABC Market
+                                        Product Description
                                     </span></h1></span>
                                 </div><!--end layout-widget-wrapper -->
                             </div><!--end widget-span -->
@@ -891,7 +906,7 @@ $services_dict = array(1 => "<a href = 'services/str_cnslt.php'>Strategy and Con
                         <div class="row-fluid ">
                             <div class="span12 widget-span widget-type-rich_text " style="" data-widget-type="rich_text" data-x="0" data-w="12">
                                 <div class="cell-wrapper layout-widget-wrapper">
-                                    <span id="hs_cos_wrapper_module_14509432659418859" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_rich_text" style="" data-hs-cos-general-type="widget" data-hs-cos-type="rich_text"><p>A Collection of Products from our Partners</p></span>
+                                    <span id="hs_cos_wrapper_module_14509432659418859" class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_rich_text" style="" data-hs-cos-general-type="widget" data-hs-cos-type="rich_text"><p>A Description of Product You Selected</p></span>
                                 </div><!--end layout-widget-wrapper -->
                             </div><!--end widget-span -->
                         </div><!--end row-->
@@ -1079,6 +1094,65 @@ $services_dict = array(1 => "<a href = 'services/str_cnslt.php'>Strategy and Con
 					//echo '</pre>';
 				?>
 			</div>    
+			
+			<div class="container">
+  <div class="row">
+    <div class="col-md-4"></div>
+	<div class="col-md-4">
+	
+		<br><br><br><br><br>
+		<!-- Button trigger modal -->
+		<button type="button" class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">Submit review</button>
+		<br><br>
+		<div id="message"></div>
+		
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title" id="myModalLabel">Review System</h4>
+					</div>
+					
+					<div class="modal-body">
+						<form method="POST" id="dataform" action="" autocomplete="true">
+							<label>Leave your Feedback</label>
+							<input id="star" value="4" type="number" class="rating" min=0 max=5 step=0.2 data-size="md">
+							<label>Comments:</label>
+							<textarea id="comment" class="form-control" placeholder="Please enter comments here about your experience with this teacher." rows="6"></textarea>
+					</div>
+					<div class="modal-footer">
+							<button type="reset" class="btn btn-default" data-dismiss="modal">Close</button>
+							<button type="submit" id="submit_rate" class="btn btn-primary">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+	<div class="col-md-4"></div>
+  </div>
+</div>
+</body>
+<script>
+$(document).ready(function(){ 
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+});
+$('#submit_rate').click(function(){
+	var data = $('#dataform').serialize();
+	var check=$.ajax({
+	type:"post",
+	url:"submit_rating.php",
+	data:data,
+	contentType:"application/x-www-form-urlencoded",
+	success: function(responseData, textStatus , jqXHR){$('#message').html(responseData);},
+	error:function(textStatus, errorThrown){console.log(errorThrown);}
+ });
+ return false;
+ });
+});
+</script>
                                     <?php
                                         
                                         
